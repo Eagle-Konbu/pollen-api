9ac918d538426d9f66e09becaaea95a334bd62f2
Atsuya Uchida <atsuya_eagle@outlook.com>
2021-03-27 10:41:52 +0900

    added handler.

--- handler.js
+++ handler.js
@@ -1,11 +1,12 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+module.exports.getPollenInfo = async (event) => {
+  const pollenInfo = await require('./src/get-pollen-info').getPollenInfo()
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: pollenInfo,
         input: event,
       },
       null,
