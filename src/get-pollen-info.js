d27fa5c06342e021e2837ffa6bc9cde41b766494
Atsuya Uchida <atsuya_eagle@outlook.com>
2021-03-27 10:25:00 +0900

    added selector.

--- src/get-pollen-info.js
+++ src/get-pollen-info.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 async function getPollenInfo() {
   const browser = await puppeteer.launch();
-  const selector = "";
+  const selector = ".pollen-telop";
   let pollenInfo = "Not found";
 
   try {
