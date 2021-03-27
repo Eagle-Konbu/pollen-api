43a912bffdf5c8c026e12c26b316ab7a9bb780a7
Atsuya Uchida <atsuya_eagle@outlook.com>
2021-03-27 11:28:12 +0900

    changed browser pos.

--- src/get-pollen-info.js
+++ src/get-pollen-info.js
@@ -4,15 +4,16 @@ const puppeteer = require("puppeteer-core");
 exports.getPollenInfo = async () => {
   const selector = ".pollen-telop";
   let pollenInfo = "Not found";
+  let browser = null;
 
   try {
-    const browser = await puppeteer.launch({
+    browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath,
       headless: chromium.headless,
     });
-    
+
     const page = await browser.newPage();
 
     await page.goto("https://tenki.jp/pollen/3/17/4610/14100/");
